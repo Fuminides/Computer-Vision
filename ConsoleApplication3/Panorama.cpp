756bfdcbf0e4c1c59a1a8bbb702b792f33e188d5
Javier Fumanal <javierfumanalidocin@gmail.com>
2017-04-20 00:01:50 +0200

    Panorama color parche juntado

--- ConsoleApplication3/Panorama.cpp
+++ ConsoleApplication3/Panorama.cpp
@@ -119,56 +119,78 @@ vector<DMatch> match_descriptors(Mat descriptors1, Mat descriptors2, int mode) {
 	return matches;
 }
 
-Mat juntarImagenes(Mat referencia_color, Mat nueva_color, Mat referencia, Mat nueva, vector<KeyPoint> k2, Mat descriptores2, int modo = USE_BRISK) {
-	vector<KeyPoint> k1;
-	vector<DMatch> matches;
-	Mat descriptores1;
-
-	k1 = compute_keypoints(nueva, modo);
-	descriptores1 = extraer_descriptores(nueva, k1, modo);
-	//La 1 se convierte en la 2
-	int y_stitch = 0, y_final = 0;
+Mat juntarImagenes_derecha(Mat referencia_color, Mat nueva_color, Mat referencia, Mat nueva, vector<KeyPoint> k2, Mat descriptores2, int ultima_columna, int modo = USE_BRISK, int filtro = 1) {
 	
+	
+	if (filtro == 2) {
+		Mat ilum_referencia, ilum_nueva;
+		vector<Mat> channels_referencia, channels_nueva;
+		Scalar media_referencia, media_nueva;
+		double proporcion;
 
-	matches = match_descriptors(descriptores2, descriptores1, USE_VECINO);
-	int matchers_size = matches.size();
+		cvtColor(referencia_color, ilum_referencia, CV_BGR2YCrCb);
+		cvtColor(nueva_color, ilum_nueva, CV_BGR2YCrCb);
+		split(ilum_referencia, channels_referencia);
+		split(ilum_nueva, channels_nueva);
+		media_referencia = mean(channels_referencia[0]);
+		media_nueva = mean(channels_nueva[0]);
+		proporcion = media_referencia[0] / media_nueva[0];
+		cout << proporcion << endl;
+		channels_nueva[0] = channels_nueva[0] * proporcion*0.80;
+		merge(channels_nueva, ilum_nueva);
+		cvtColor(ilum_nueva, nueva_color, CV_YCrCb2BGR);
+	}
 
-	for (int i = 0; i < matchers_size; i++) {
-		Point2f good_match = k2[matches[i].queryIdx].pt;
-		if (good_match.y > y_final) {
-			y_final = good_match.y;
-			y_stitch = k1[matches[i].trainIdx].pt.y;
-
+	nueva.resize(referencia.rows);
+	nueva_color.resize(referencia.rows);
+	double alpha = 0.5;
+	Mat imagenFinal(referencia.rows, referencia.cols + nueva.cols, DataType<unsigned char>::type);
+	Mat imagenFinal_color(referencia.rows, referencia.cols + nueva.cols, CV_8UC3);
+	int posicion = -1;
+	for (int i = 1; i < referencia.rows; ++i) {
+		for (int j = 1; j < ultima_columna; ++j) {
+			if (filtro == 1) {
+				if ((nueva.at<unsigned char>(i, j) != 0) && (j < referencia.cols - 1)) {
+					if (posicion < 0) {
+						double espacio = referencia.cols - j;
+						alpha = 1 / espacio;
+						posicion = j;
+					}
+					imagenFinal.at<unsigned char>(i, j) = referencia.at<unsigned char>(i, j);
+					imagenFinal_color.at<Vec3b>(i, j) = nueva_color.at<Vec3b>(i, j)*(alpha*abs(j - posicion)) + referencia_color.at<Vec3b>(i, j)*(1 - (alpha*abs(j - posicion)));
+				}
+				else if (j < referencia.cols - 1) {
+					imagenFinal_color.at<Vec3b>(i, j) = referencia_color.at<Vec3b>(i, j);
+				}
+				else {
+					imagenFinal_color.at<Vec3b>(i, j) = nueva_color.at<Vec3b>(i, j);
+				}
+			}
+			else if (filtro == 2) {
+				if (j < referencia.cols - 1) {
+					imagenFinal_color.at<Vec3b>(i, j) = referencia_color.at<Vec3b>(i, j);
+				}
+				else {
+					imagenFinal_color.at<Vec3b>(i, j) = nueva_color.at<Vec3b>(i, j);
+				}
+			}
 		}
 	}
-	cout << "y_final" << y_final << " y empalme " << y_stitch << endl;
 
-	Mat imagenFinal(referencia.rows, y_final + nueva.cols - y_stitch, DataType<unsigned char>::type);
-	for (int i = 0; i < imagenFinal.rows; ++i) {
-		for (int j = 0; j < imagenFinal.cols; ++j) {
-			if (j > y_final) {
-				imagenFinal.at<unsigned char>(i, j) = nueva.at<unsigned char>(i, j - y_final + y_stitch);
-			}
-			else {
-				imagenFinal.at<unsigned char>(i, j) = referencia.at<unsigned char>(i, j);
-			}
-		}
-	}
-	namedWindow("Panoramix", 4);
-	imshow("Panoramix", imagenFinal);
+	imagenFinal_color = imagenFinal_color.colRange(0, ultima_columna);
+	cv::namedWindow("Panoramix", 7);
+	cv::imshow("Panoramix", imagenFinal_color);
 
-	waitKey(0);
+	cv::waitKey(0);
 
 	return imagenFinal;
 }
 
-void matching_disco(int argc, char ** argv, bool full=false) {
-	Mat imagen1 = imread(argv[2], CV_LOAD_IMAGE_GRAYSCALE);
-	Mat imagen2 = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+void match_images(Mat imagen1, Mat imagen1_color, Mat imagen2, Mat imagen2_color, bool full = true) {
 	vector<KeyPoint> k1, k2;
 	vector<Point2f> puntos1, puntos2;
 	vector<DMatch> matches, inliers;
-	Mat mask, homografia, descriptores1, descriptores2, composicion, inliers_figure;
+	Mat mask, homografia, descriptores1, descriptores2, composicion, composicion_color, inliers_figure;
 
 	k1 = compute_keypoints(imagen1, USE_BRISK);
 	k2 = compute_keypoints(imagen2, USE_BRISK);
@@ -193,12 +215,22 @@ void matching_disco(int argc, char ** argv, bool full=false) {
 		namedWindow("inliers", 1);
 		drawMatches(imagen1, k1, imagen2, k2, inliers, inliers_figure);
 		imshow("inliers", inliers_figure);
-		
-		warpPerspective(imagen1, composicion, homografia, imagen2.size());
+		warpPerspective(imagen1, composicion, homografia, Size(imagen1.cols+imagen2.cols, imagen1.rows));
+		warpPerspective(imagen1_color, composicion_color, homografia, Size(imagen1.cols + imagen2.cols, imagen1.rows));
+
 		namedWindow("Homografia", 2);
 		imshow("Homografia", composicion);
-
-		juntarImagenes(imagen2, imagen2, imagen2, composicion, k2, descriptores2, USE_BRISK);
+		int suma_ultima_columna = 0;
+		vector<int> v_sumas;
+		reduce(composicion, v_sumas, 0, CV_REDUCE_SUM, -1);
+		int tam = v_sumas.size(), ultima_columna;
+		for (int z = 1; z < tam-1; z++) {
+			if ((v_sumas[z] == 0) && (v_sumas[z+1] == 0) && (v_sumas[z-1] != 0)) {
+				ultima_columna = z - 1;
+				break;
+			}
+		}
+		juntarImagenes_derecha(imagen2_color, composicion_color, imagen2, composicion, k2, descriptores2, ultima_columna, USE_BRISK);
 
 	}
 	else {
@@ -208,11 +240,15 @@ void matching_disco(int argc, char ** argv, bool full=false) {
 		imshow("matches", img_matches);
 		waitKey(0);
 	}
-	
-
 
 }
 
+void matching_disco(int argc, char ** argv, bool full = false) {
+	Mat imagen1 = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE), imagen1_color = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+	Mat imagen2 = imread(argv[2], CV_LOAD_IMAGE_GRAYSCALE), imagen2_color = imread(argv[2], CV_LOAD_IMAGE_COLOR);
+
+	match_images(imagen1, imagen1_color, imagen2, imagen2_color);
+}
 void main(int argc, char ** argv) {
 	matching_disco(argc, argv, true);
 }
